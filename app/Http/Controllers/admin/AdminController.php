e820a1384678cc2133bcf5ade5905fd377860558
sheikhnayan <sheikhnayan1997@gmail.com>
2024-01-11 21:23:08 +0600

    test

--- app/Http/Controllers/admin/AdminController.php
+++ app/Http/Controllers/admin/AdminController.php
@@ -11,6 +11,13 @@ class AdminController extends Controller
 {
     public function index()
     {
+        if (Auth::user()->type == 'admin') {
+            # code...
+            if (Auth::user()->stripe_id == null) {
+                # code...
+                return redirect('/plans');
+            }
+        }
         $team = Team::where('user_id',Auth::user()->id)->get();
 
         return view('admin.index',compact('team'));
